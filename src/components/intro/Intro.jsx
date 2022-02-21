115f877f08100891f4ecb45a113371e1f4a45663
Estefany Silfa <esilfa3@outlook.com>
2022-02-21 01:56:10 -0500

    add introl

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,30 @@
 import './intro.css'
+import Typed from "react-typed"
 
 export default function intro() {
     return (
-        <div className='intro'>
-      
-            
+        <div className="header-waper">
+            <div className='intro'>
+                <h1
+                typeSpeed={50}
+                backSpeed={40}
+                loop>Hello! I am Estefany  </h1>
+                <Typed 
+                className = "typed-text"
+                strings={[  "Web developer "  ]}
+                typeSpeed={70}
+                backSpeed={80}
+                loop
+                
+                />
+                
+                
+
+
+            </div>
+
+
         </div>
+
     )
 }
